5bfa6d4392585b475c92a989277d61205c161715
Jesper Friis <jesper.friis@sintef.no>
2019-12-05 15:36:23 +0100

    Updated owldir.py

--- emmo/owldir.py
+++ emmo/owldir.py
@@ -9,12 +9,19 @@ import owlready2
 
 
 thisdir = os.path.abspath(os.path.realpath((os.path.dirname(__file__))))
-owldir = os.path.abspath(os.path.join(thisdir, 'rdfxml'))
+owldir = os.path.abspath(os.path.join(thisdir, '..', 'owl'))
 
-if not os.path.exists(os.path.join(owldir, 'emmo.owl')):
+help_message = '''\
+Maybe you are working on a git repository and haven't updated the EMMO
+submodule in the owl subdirectory.  Try to run the following commands:
+
+    git submodule init
+    git submodule update
+'''
+
+if not os.path.exists(os.path.join(owldir, 'emmo-inferred.owl')):
     print('File does not exists: ', os.path.join(owldir, 'emmo.owl'))
-    print('You should install it by running the `copyowl.py` script')
-    print('in the python root directory.')
+    print(help_message)
     exit(1)
 
 owlready2.onto_path.append(owldir)
