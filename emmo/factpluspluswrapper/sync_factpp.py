46ab312b3bf42191361e43d27a8e4d8acb400c13
Jesper Friis <jesper.friis@sintef.no>
2020-12-31 13:02:01 +0100

    Implemented sync_reasoner_factpp() - seems to work

--- emmo/factpluspluswrapper/sync_factpp.py
+++ emmo/factpluspluswrapper/sync_factpp.py
@@ -1,77 +1,109 @@
-import tempfile
+import collections
 
-from rdflib import OWL, RDFS
+import rdflib
+from rdflib import URIRef, RDF, RDFS, OWL
 
+import owlready2
 from owlready2 import World, Ontology, CURRENT_NAMESPACES
-from owlready2.reasoning import _INFERRENCES_ONTOLOGY
+from owlready2.reasoning import (
+    _apply_reasoning_results, _apply_inferred_obj_relations,
+    _INFERRENCES_ONTOLOGY)
 
-from .owlapi_interface import OwlApiInterface
+from .factppgraph import FaCTPPGraph
 
 
-def sync_reasoner_factpp(x=None, infer_property_values=False, debug=1, keep_tmp_file=False):
+OWL_2_TYPE = {
+    RDFS.subClassOf:        'class',
+    RDFS.subPropertyOf:     'property',
+    RDF.type:               'individual',
+    OWL.equivalentClass:    'class',
+    OWL.equivalentProperty: 'property',
+}
+
+
+def sync_reasoner_factpp(ontology_or_world=None, infer_property_values=False,
+                         debug=1):
+    """Run FaCT++ reasoner and load the inferred relations back into
+    the owlready2 triplestore.
+
+    Parameters
+    ----------
+    ontology_or_world : None | Ontology instance | World instance | list
+        Identifies the world to run the reasoner over.
+    infer_property_values : bool
+        Whether to also infer property values.
+    debug : bool
+        Whether to print debug info to standard output.
     """
-    """
-    if isinstance(x, World):
-        world = x
-    elif isinstance(x, Ontology):
-        world = x.world
-    elif isinstance(x, list):
-        world = x[0].world
+    if isinstance(ontology_or_world, World):
+        world = ontology_or_world
+    elif isinstance(ontology_or_world, Ontology):
+        world = ontology_or_world.world
+    elif isinstance(ontology_or_world, collections.Sequence):
+        world = ontology_or_world[0].world
     else:
         world = owlready2.default_world
 
+    if isinstance(ontology_or_world, Ontology):
+        ontology = ontology_or_world
+    elif CURRENT_NAMESPACES.get():
+        ontology = CURRENT_NAMESPACES.get()[-1].ontology
+    else:
+        ontology = world.get_ontology(_INFERRENCES_ONTOLOGY)
+
     locked = world.graph.has_write_lock()
     if locked:
-        world.graph.release_write_lock() # Not needed during reasoning
+        world.graph.release_write_lock()  # Not needed during reasoning
 
     try:
+        print('*** Prepare graph')
+        # Exclude owl:imports because they are not needed and can
+        # cause trouble when loading the inferred ontology
+        g1 = rdflib.Graph()
+        for s, p, o in world.as_rdflib_graph().triples((None, None, None)):
+            if p != OWL.imports:
+                g1.add((s, p, o))
 
-        #if isinstance(x, Ontology):
-        #    ontology = x
-        #elif CURRENT_NAMESPACES.get():
-        #    ontology = CURRENT_NAMESPACES.get()[-1].ontology
-        #else:
-        #    ontology = world.get_ontology(_INFERRENCES_ONTOLOGY)
+        print('*** Run FaCT++ reasoner (and postprocess)')
+        g2 = FaCTPPGraph(g1).inferred_graph()
 
-        #f = tempfile.NamedTemporaryFile("wb", delete=False)
-        #if isinstance(x, list):
-        #    for o in x:
-        #        o.save(f, format="ntriples", commit=False)
-        #else:
-        #    world.save(f, format="ntriples")
-        #f.close()
+        print('*** Load inferred ontology')
+        # Check all rdfs:subClassOf relations in the inferred graph and add
+        # them to the world if they are missing
+        new_parents = collections.defaultdict(list)
+        new_equivs = collections.defaultdict(list)
+        entity_2_type = {}
 
-        print('*** start reasoning')
-        g1 = world.as_rdflib_graph()
+        for s, p, o in g2.triples((None, None, None)):
+            if (isinstance(s, URIRef) and
+                    p in OWL_2_TYPE and
+                    isinstance(o, URIRef)):
+                s_storid = ontology._abbreviate(str(s), False)
+                p_storid = ontology._abbreviate(str(p), False)
+                o_storid = ontology._abbreviate(str(o), False)
+                if (s_storid is not None and
+                        p_storid is not None and
+                        o_storid is not None):
+                    if p in (RDFS.subClassOf, RDFS.subPropertyOf, RDF.type):
+                        new_parents[s_storid].append(o_storid)
+                        entity_2_type[s_storid] = OWL_2_TYPE[p]
+                    else:
+                        new_equivs[s_storid].append(o_storid)
+                        entity_2_type[s_storid] = OWL_2_TYPE[p]
 
-        # Remove owl:imports since they may cause problem when loading
-        # the reasoned ontology
-        for t in g1.triples((None, OWL.imports, None)):
-            g1.remove(t)
-
-        interface = OwlApiInterface()
-        g2 = interface.reason(g1)
-
-        print('*** start diffing')
-        #both, only_g1, only_g2 = graph_diff(g1, g2)
-        s1 = set(g1.triples((None, RDFS.subClassOf, None)))
-        s2 = set(g2.triples((None, RDFS.subClassOf, None)))
-
-        with open('diff.txt', 'wt') as f:
-            from emmo.graph import getlabel
-            for s, p, o in s2.difference(s1):
-                #olabel = getlabel(world[str(o)])
-                #f.write('%-40s owl:subClassOf  %s\n' % (
-                #    getlabel(world[str(s)]), olabel if olabel != 'None' else o))
-                f.write('%-60s owl:subClassOf %s\n' % (s, o))
-
-        g1.serialize('g1.ttl', format='turtle')
-        g2.serialize('g2.ttl', format='turtle')
-
-
-        print('*** done diffing')
-
+        if infer_property_values:
+            inferred_obj_relations = []
+            # Hmm, does FaCT++ infer any property values?
+            # If not, remove the `infer_property_values` keyword argument.
+            raise NotImplementedError
 
     finally:
         if locked:
-            world.graph.acquire_write_lock() # re-lock when applying results
+            world.graph.acquire_write_lock()  # re-lock when applying results
+
+    print('*** Applying reasoning results')
+    _apply_reasoning_results(world, ontology, debug, new_parents, new_equivs,
+                             entity_2_type)
+    if infer_property_values:
+        _apply_inferred_obj_relations(world, ontology, debug,
+                                      inferred_obj_relations)
