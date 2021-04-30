d0ebbc9bfeae4f499f82d91e1a4aa5d31947197e
francescalb <francesca.l.bleken@sintef.no>
2021-04-30 18:56:41 +0200

    Two lines before new functio in utils

--- emmo/utils.py
+++ emmo/utils.py
@@ -51,6 +51,7 @@ def get_label(e):
     else:
         return repr(e)
 
+
 def asstring(expr, link='{name}', n=0, exclude_object=False):
     """Returns a string representation of `expr`, which may be an entity,
     restriction, or logical expression of these.  `link` is a format
