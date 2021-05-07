70c87a1328b64b3eba32e57a79edfe50d428c204
Jesper Friis <jesper.friis@sintef.no>
2021-05-07 11:12:23 +0200

    Yet another fix

--- emmo/tests/test_load_emmo.py
+++ emmo/tests/test_load_emmo.py
@@ -1,9 +1,11 @@
 # Add emmo to sys path
+import os
+import sys
+
 thisdir = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(1, os.path.abspath(os.path.join(thisdir, '..', '..')))
 from emmo import get_ontology  # noqa: E402, F401
 
 
-emmo = get_ontology(
-    'https://raw.githubusercontent.com/emmo-repo/EMMO/v1.0.0/emmo.owl')
+emmo = get_ontology()
 emmo.load()
