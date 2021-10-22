729f5a9a484d6e21863df19b26ff6a35feebb9a0
francescalb <francesca.l.bleken@sintef.no>
2021-10-22 12:14:53 +0200

    Moved test_rdfs function to test_load.py file

--- tests/test_load.py
+++ tests/test_load.py
@@ -32,12 +32,16 @@ def test_load(repo_dir: "Path") -> None:
         'battinfo.ttl').load()
     assert onto.Electrolyte.prefLabel.first() == 'Electrolyte'
 
-    # Test loading of non-EMMObased ontology
+
+def test_load_rdfs(repo_dir: "Path") -> None:
+    from ontopy import get_ontology
+
+    # Test loading non-EMMO-based ontologies rdf and rdfs
     rdf_onto = get_ontology(
         'https://www.w3.org/1999/02/22-rdf-syntax-ns.ttl').load(
             EMMObased=False)
     rdfs_onto = get_ontology(
         'https://www.w3.org/2000/01/rdf-schema.ttl').load(
             EMMObased=False)
-    rdfs_onto.Class #  Needed to initialize rdfs_onto
+    rdfs_onto.Class  # Needed to initialize rdfs_onto
     assert type(rdf_onto.HTML).iri == rdfs_onto.Datatype.iri
