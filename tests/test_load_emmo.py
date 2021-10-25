61011d5fa2e05f1429d8ad2cf613e07359550ecc
francescalb <francesca.l.bleken@sintef.no>
2021-10-25 23:02:53 +0200

    Added testing of new relation

--- tests/test_load_emmo.py
+++ tests/test_load_emmo.py
@@ -7,14 +7,19 @@ def test_load_emmo() -> None:
     from emmopy import get_emmo
 
     EMMO_inferred = get_emmo()
+    EMMO_inf = get_emmo(None)
     EMMO = get_emmo(inferred=False)
 
     assert EMMO_inferred
     assert EMMO
 
-    assert EMMO_inferred == get_emmo(None)
+    assert EMMO_inferred == EMMO_inf
     assert EMMO != EMMO_inferred
 
     EMMO_inferred.new_entity('HydrogenAtom', EMMO_inferred.Atom)
     EMMO_inferred.sync_attributes()
-    assert EMMO_inferred != get_emmo(None)
+    assert EMMO_inferred != EMMO_inf
+
+    EMMO_inferred2 = get_emmo()
+    EMMO_inferred2.Atom.comment.append('New triple')
+    assert EMMO_inferred2 != EMMO_inf
