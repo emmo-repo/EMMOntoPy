b7371d089d5003ef54c1ef4f5395b710a71e6e38
Francesca L. Bleken <48128015+francescalb@users.noreply.github.com>
2021-11-01 13:32:51 +0100

    Name of inferred ontology not shortened any longer
    Co-authored-by: Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>

--- tests/test_load_emmo.py
+++ tests/test_load_emmo.py
@@ -7,19 +7,20 @@ def test_load_emmo() -> None:
     from emmopy import get_emmo
 
     EMMO_inferred = get_emmo()
-    EMMO_inf = get_emmo(None)
+    EMMO_inferred_again = get_emmo(None)
     EMMO = get_emmo(inferred=False)
 
     assert EMMO_inferred
+    assert EMMO_inferred_again
     assert EMMO
 
-    assert EMMO_inferred == EMMO_inf
+    assert EMMO_inferred == EMMO_inferred_again
     assert EMMO != EMMO_inferred
 
     EMMO_inferred.new_entity('HydrogenAtom', EMMO_inferred.Atom)
     EMMO_inferred.sync_attributes()
-    assert EMMO_inferred != EMMO_inf
+    assert EMMO_inferred != EMMO_inferred_again
 
     EMMO_inferred2 = get_emmo()
     EMMO_inferred2.Atom.comment.append('New triple')
-    assert EMMO_inferred2 != EMMO_inf
+    assert EMMO_inferred2 != EMMO_inferred_again
