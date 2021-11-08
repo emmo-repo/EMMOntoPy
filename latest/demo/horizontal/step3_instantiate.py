29ac622d63e6d49d9e9ebd86d629578f35484be1
EMMOntoPy Developers <EMMOntoPy@emmo.info>
2021-11-08 13:50:02 +0000

    Deployed d5ae907 to latest with MkDocs 1.2.3 and mike 1.1.2

--- latest/demo/horizontal/step3_instantiate.py
+++ latest/demo/horizontal/step3_instantiate.py
@@ -7,6 +7,7 @@ In this step we uses the Atomistic Simulation Environment (ASE) to load
 a atomistic Al-Fe4Al13 interface structure from a cif file and
 represents it using the metadata defined in step 2.
 """
+# pylint: disable=import-error
 import ase
 import ase.io
 
@@ -27,14 +28,14 @@ from step2_define_metadata import DLiteAtoms
 
 
 # Load atom structure from cif file
-at = ase.io.read('../vertical/Al-Fe4Al13.cif')
+at = ase.io.read("../vertical/Al-Fe4Al13.cif")
 
 # convert atom structure to a DLiteAtoms object
-atoms = dlite.objectfactory(at, cls=DLiteAtoms, instanceid='atoms_Al-Fe4Al13')
+atoms = dlite.objectfactory(at, cls=DLiteAtoms, instanceid="atoms_Al-Fe4Al13")
 
 
 # Create a new collection for data instances
-coll = dlite.Collection('usercase_appdata')
-coll.add('Atoms', atoms.dlite_meta)
-coll.add('atoms', atoms.dlite_inst)
-coll.save('json', 'usercase_appdata.json', 'mode=w')
+coll = dlite.Collection("usercase_appdata")
+coll.add("Atoms", atoms.dlite_meta)
+coll.add("atoms", atoms.dlite_inst)
+coll.save("json", "usercase_appdata.json", "mode=w")
