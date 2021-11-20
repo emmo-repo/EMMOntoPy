da6c9e571321437e3f9c9ff6268a13ee1124ec1f
Jesper Friis <jesper.friis@sintef.no>
2021-11-20 19:30:48 +0100

    Corrected bug in __init__ modules in emmopy and ontopy and cleaned up test_manchester.py

--- ontopy/__init__.py
+++ ontopy/__init__.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 6):
     raise RuntimeError("ontopy requires Python 3.6 or later")
 
 # Ensure ontopy is imported before owlready2...
-if "owlready2" in sys.modules and "emmopy" not in sys.modules:
+if "owlready2" in sys.modules and "ontopy" not in sys.modules:
     raise RuntimeError("ontopy must be imported before owlready2")
 
 # Monkey patch Owlready2 by injecting some methods
--- tests/test_manchester.py
+++ tests/test_manchester.py
@@ -3,41 +3,30 @@ from ontopy.manchester import evaluate
 from owlready2 import And, Or, Not, Inverse
 
 
-def test(s, expected, no_traceback=True):
-    if no_traceback:
-        try:
-            r = evaluate(emmo, s)
-        except pp.ParseException as e:
-            print('**', s, f'-- ParseError: {e}')
-        except Exception as e:
-            print('**', s, f'-- EvalError: {e}')
-    else:
-        r = evaluate(emmo, s)
-
-    if repr(r) == repr(expected):
-        print(s, f'--> {r}')
-    else:
-        print('**', s, f'-- Failed: {r} != {expected}')
+def check(s, expected):
+    r = evaluate(emmo, s)
+    print(s, '-->', r)
+    assert repr(r) == repr(expected)
 
 
 emmo = get_ontology().load()
 
-test('Item', emmo.Item)
-test('not Item', Not(emmo.Item))
-test('not not Item', emmo.Item)
-test('hasPart some Atom', emmo.hasPart.some(emmo.Atom))
-test('Atom and not Molecule', emmo.Atom & Not(emmo.Molecule))
-test('Atom and (not Molecule)', emmo.Atom & Not(emmo.Molecule))
-test('not Atom and Molecule', Not(emmo.Atom) & emmo.Molecule)
-test('(not Atom) and Molecule', Not(emmo.Atom) & emmo.Molecule)
-test('inverse hasPart some Atom', Inverse(emmo.hasPart).some(emmo.Atom))
-test('inverse(hasPart) some Atom', Inverse(emmo.hasPart).some(emmo.Atom))
-test('not hasPart some Atom', Not(emmo.hasPart.some(emmo.Atom)))
-test('not (hasPart some Atom)', Not(emmo.hasPart.some(emmo.Atom)))
-test('hasPart some (not Atom)', emmo.hasPart.some(Not(emmo.Atom)))
-test('hasPart some not Atom', emmo.hasPart.some(Not(emmo.Atom)))
-test('not hasPart some not Atom', Not(emmo.hasPart.some(Not(emmo.Atom))))
-test('hasPart only (inverse hasPart some not Atom)',
+check('Item', emmo.Item)
+check('not Item', Not(emmo.Item))
+check('not not Item', emmo.Item)
+check('hasPart some Atom', emmo.hasPart.some(emmo.Atom))
+check('Atom and not Molecule', emmo.Atom & Not(emmo.Molecule))
+check('Atom and (not Molecule)', emmo.Atom & Not(emmo.Molecule))
+check('not Atom and Molecule', Not(emmo.Atom) & emmo.Molecule)
+check('(not Atom) and Molecule', Not(emmo.Atom) & emmo.Molecule)
+check('inverse hasPart some Atom', Inverse(emmo.hasPart).some(emmo.Atom))
+check('inverse(hasPart) some Atom', Inverse(emmo.hasPart).some(emmo.Atom))
+check('not hasPart some Atom', Not(emmo.hasPart.some(emmo.Atom)))
+check('not (hasPart some Atom)', Not(emmo.hasPart.some(emmo.Atom)))
+check('hasPart some (not Atom)', emmo.hasPart.some(Not(emmo.Atom)))
+check('hasPart some not Atom', emmo.hasPart.some(Not(emmo.Atom)))
+check('not hasPart some not Atom', Not(emmo.hasPart.some(Not(emmo.Atom))))
+check('hasPart only (inverse hasPart some not Atom)',
      emmo.hasPart.only(Inverse(emmo.hasPart).some(Not(emmo.Atom))))
-test('hasPart only inverse hasPart some not Atom',
+check('hasPart only inverse hasPart some not Atom',
      emmo.hasPart.only(Inverse(emmo.hasPart).some(Not(emmo.Atom))))
