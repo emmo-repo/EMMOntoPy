c44a63b602112550f80f0ed737c3eb27558821a8
francescalb <francesca.l.bleken@sintef.no>
2021-12-10 10:28:03 +0100

    Corrected path in test

--- tests/test_excelparser.py
+++ tests/test_excelparser.py
@@ -2,13 +2,14 @@ from ontopy import get_ontology
 from ontopy.excelparser import create_ontology_from_excel
 from ontopy.utils import write_catalog
 
-onto = get_ontology("testonto/excelparser/fromexcelonto.ttl").load()
 
-
-def test_excelparser():
-    ontology, catalog = create_ontology_from_excel(
-        "testonto/excelparser/onto.xlsx"
+def test_excelparser(repo_dir: "Path") -> None:
+    ontopath = (
+        repo_dir / "tests" / "testonto" / "excelparser" / "fromexcelonto.ttl"
     )
+    onto = get_ontology(str(ontopath)).load()
+    xlspath = repo_dir / "tests" / "testonto" / "excelparser" / "onto.xlsx"
+    ontology, catalog = create_ontology_from_excel(xlspath)
     assert onto == ontology
 
 
