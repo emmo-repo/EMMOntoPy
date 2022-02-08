73474bc0d883b333b5af49c51790709d5a83e749
Sylvain Gouttebroze <sylvain.gouttebroze@sintef.no>
2022-02-08 10:55:02 +0100

    fix #365 add warning and convert nan to None

--- ontopy/excelparser.py
+++ ontopy/excelparser.py
@@ -282,9 +282,12 @@ def create_ontology_from_pandas(  # pylint:disable=too-many-locals,too-many-bran
                     )
 
     # Add properties in a second loop
+    
     for index in added_rows:
         row = data.loc[index]
         properties = row["Relations"]
+        if properties == "nan":
+            properties = None
         if isinstance(properties, str):
             try:
                 concept = onto.get_by_label(row["prefLabel"].strip())
@@ -304,8 +307,12 @@ def create_ontology_from_pandas(  # pylint:disable=too-many-locals,too-many-bran
                     if force is True:
                         pass
                     else:
+                        warnings.warn(
+                        f"Error in Property assignment for: {concept}. "
+                        f"Property to be Evaluated: {prop}. "
+                        f"Error is {exc}.")
                         raise ExcelError(exc) from exc
-
+    
     # Synchronise Python attributes to ontology
     onto.sync_attributes(
         name_policy="uuid", name_prefix="EMMO_", class_docstring="elucidation"
