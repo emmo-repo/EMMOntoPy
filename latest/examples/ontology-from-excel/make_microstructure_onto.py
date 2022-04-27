7123d02553c74406e7e4e71d8d53e2d5945bcf06
EMMOntoPy Developers <Team4.0@SINTEF.no>
2022-04-27 07:28:57 +0000

    Deployed 5cdb24e to latest with MkDocs 1.3.0 and mike 1.1.2

--- latest/examples/ontology-from-excel/make_microstructure_onto.py
+++ latest/examples/ontology-from-excel/make_microstructure_onto.py
@@ -4,7 +4,9 @@ python example for creating ontology from excel
 from ontopy.excelparser import create_ontology_from_excel
 from ontopy.utils import write_catalog
 
-ontology, catalog = create_ontology_from_excel("tool/microstructure.xlsx")
+ontology, catalog, errdict = create_ontology_from_excel(
+    "tool/microstructure.xlsx"
+)
 
 ontology.save("microstructure_ontology.ttl", format="turtle", overwrite=True)
 write_catalog(catalog)
