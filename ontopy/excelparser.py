5a3c56378b659f634b7a45027ebd307ecedec1e1
Francesca L. Bleken <francesca.l.bleken@sintef.no>
2022-08-04 12:53:19 +0200

    Strip leading and trailing white spaces in path to imported ontologies in excelparser

--- ontopy/excelparser.py
+++ ontopy/excelparser.py
@@ -107,7 +107,7 @@ def create_ontology_from_excel(  # pylint: disable=too-many-arguments
     except ValueError:
         pass
     else:
-        imports.extend(imports_frame["Imported ontologies"].to_list())
+        imports.extend(imports_frame["Imported ontologies"].str.strip().to_list())
 
     # Read datafile TODO: Some magic to identify the header row
     conceptdata = pd.read_excel(
