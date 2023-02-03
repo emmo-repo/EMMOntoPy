509aea73edc376ae3948c6673eba8425d937b287
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2023-02-03 10:37:06 +0100

    Update fixtures for testing tools
    Note, only the ontodoc tests have been updated, so all other tool tests
    will fail.

--- tests/tools/conftest.py
+++ tests/tools/conftest.py
@@ -5,38 +5,100 @@ import pytest
 
 if TYPE_CHECKING:
     from types import ModuleType
-    from typing import Any, Dict
+    from typing import Any, Callable, Dict
 
 
-@pytest.fixture
-def tool(request: "Dict[str, Any]") -> "ModuleType":
-    """Import a tool as a module."""
+@pytest.fixture(scope="module", autouse=True)
+def rename_tools() -> None:
+    """Add a `.py` extension to all tools.
+
+    Run prior to all tests in this module.
+    """
     from copy import deepcopy
-    import importlib
     import os
     from pathlib import Path
+    import shutil
     import sys
 
     original_sys_path = deepcopy(sys.path)
-    original_tool_path: Path = (
-        Path(__file__).resolve().parent.parent.parent / "tools" / request.param
-    )
-    if str(original_tool_path.parent) not in sys.path:
-        sys.path.append(str(original_tool_path.parent))
+    tools_path: Path = Path(__file__).resolve().parent.parent.parent / "tools"
 
-    content_parent = "\n".join(
-        str(_) for _ in os.walk(original_tool_path.parent)
-    )
-    assert (
-        original_tool_path.exists()
-    ), f"The requested tool ({request.param}) was not found in {original_tool_path.parent}.\nContents:\n{content_parent}"
-    tool_path = None
-    try:
-        tool_path = original_tool_path.rename(
-            original_tool_path.with_suffix(".py")
-        )
-        yield importlib.import_module(request.param)
-    finally:
-        if tool_path and tool_path.exists():
-            tool_path.rename(tool_path.with_suffix(""))
-        sys.path = original_sys_path
+    if str(tools_path) not in sys.path:
+        sys.path.append(str(tools_path))
+
+    # Add ".py" suffix to all tools
+    for (
+        dirpath,
+        dirnames,
+        filenames,
+    ) in os.walk(tools_path):
+        if dirpath != str(tools_path):
+            continue
+
+        if dirnames:
+            for dirname in dirnames:
+                if dirname == "__pycache__":
+                    shutil.rmtree(
+                        Path(dirpath) / "__pycache__", ignore_errors=True
+                    )
+
+        for filename in filenames:
+            filepath = Path(dirpath) / filename
+            assert (
+                filepath.suffix == ""
+            ), f"A suffix was found (not expected) for file: {filepath}"
+
+            filepath.rename(filepath.with_suffix(".py"))
+
+    yield
+
+    # Remove ".py" suffix from all tools
+    for (
+        dirpath,
+        dirnames,
+        filenames,
+    ) in os.walk(tools_path):
+        if dirpath != str(tools_path):
+            continue
+
+        if dirnames:
+            for dirname in dirnames:
+                if dirname == "__pycache__":
+                    shutil.rmtree(
+                        Path(dirpath) / "__pycache__", ignore_errors=True
+                    )
+
+        for filename in filenames:
+            filepath = Path(dirpath) / filename
+            assert (
+                filepath.suffix == ".py"
+            ), f"A suffix was NOT found (not expected) for file: {filepath}"
+
+            filepath.rename(filepath.with_suffix(""))
+
+    sys.path = original_sys_path
+
+
+@pytest.fixture
+def get_tool() -> "Callable[[str], ModuleType]":
+    """Import a tool as a module."""
+    import importlib
+    from pathlib import Path
+    import sys
+
+    def _get_tool(name: str) -> "ModuleType":
+        """Import and return named tool."""
+        tool_path: Path = (
+            Path(__file__).resolve().parent.parent.parent / "tools" / name
+        ).with_suffix(".py")
+        assert (
+            str(tool_path.parent) in sys.path
+        ), f"'tools' dir not found in sys.path. Did `rename_tools` fixture run?\nsys.path: {sys.path}"
+
+        assert (
+            tool_path.exists()
+        ), f"The requested tool ({name}) was not found in {tool_path.parent}."
+
+        return importlib.import_module(name)
+
+    return _get_tool
--- tests/tools/test_ontodoc.py
+++ tests/tools/test_ontodoc.py
@@ -1,52 +1,67 @@
 """Test the `ontodoc` tool."""
-from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pytest
 
+if TYPE_CHECKING:
+    from pathlib import Path
+    from types import ModuleType
+    from typing import Callable
 
-@pytest.mark.parametrize("tool", ["ontodoc"], indirect=True)
-def test_run(tool, tmpdir: Path) -> None:
+
+def test_run(get_tool: "Callable[[str], ModuleType]", tmpdir: "Path") -> None:
     """Check that running `ontodoc` works."""
+    from pathlib import Path
+
     test_file = (
         Path(__file__).resolve().parent.parent / "testonto" / "models.ttl"
     )
+    ontodoc = get_tool("ontodoc")
 
-    tool.main([str(test_file), str(tmpdir / "test.md")])
-    tool.main(
+    ontodoc.main([str(test_file), str(tmpdir / "test.md")])
+    ontodoc.main(
         [str(test_file), "--format=simple-html", str(tmpdir / "test.html")]
     )
 
 
-@pytest.mark.parametrize("tool", ["ontodoc"], indirect=True)
-def test_run_w_individual(tool, tmpdir: Path) -> None:
+def test_run_w_individual(
+    get_tool: "Callable[[str], ModuleType]", tmpdir: "Path"
+) -> None:
     """Check that running `ontodoc` works when there is an individual."""
+    from pathlib import Path
+
     test_file = (
         Path(__file__).resolve().parent.parent
         / "testonto"
         / "testonto_w_individual.ttl"
     )
+    ontodoc = get_tool("ontodoc")
 
-    tool.main([str(test_file), str(tmpdir / "test.md")])
-    tool.main(
+    ontodoc.main([str(test_file), str(tmpdir / "test.md")])
+    ontodoc.main(
         [str(test_file), "--format=simple-html", str(tmpdir / "test.html")]
     )
 
 
-@pytest.mark.parametrize("tool", ["ontodoc"], indirect=True)
 @pytest.mark.filterwarnings(
     "ignore:Ignoring instance"
 )  # currently pytest is set to accept warnings, but this might change in the future
-def test_run_w_punning(tool, tmpdir: Path) -> None:
+def test_run_w_punning(
+    get_tool: "Callable[[str], ModuleType]", tmpdir: "Path"
+) -> None:
     """Check that running `ontodoc` works even if there is a punned individual.
     This will throw and extra warning as the punned individual will be ignored.
     """
+    from pathlib import Path
+
     test_file = (
         Path(__file__).resolve().parent.parent
         / "testonto"
         / "testonto_w_punning.ttl"
     )
+    ontodoc = get_tool("ontodoc")
 
-    tool.main([str(test_file), str(tmpdir / "test.md")])
-    tool.main(
+    ontodoc.main([str(test_file), str(tmpdir / "test.md")])
+    ontodoc.main(
         [str(test_file), "--format=simple-html", str(tmpdir / "test.html")]
     )
