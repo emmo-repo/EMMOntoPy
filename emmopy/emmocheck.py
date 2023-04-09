b33319c8caa06fc107738f9c31ae7195a8fdf747
Jesper Friis <jesper.friis@sintef.no>
2023-04-10 00:17:33 +0200

    Added two additional exceptions to emmocheck

--- emmopy/emmocheck.py
+++ emmopy/emmocheck.py
@@ -82,6 +82,7 @@ class TestSyntacticEMMOConventions(TestEMMOConventions):
                 "core.prefLabel",
                 "core.altLabel",
                 "core.hiddenLabel",
+                "foaf.logo",
                 "0.1.logo",  # foaf.logo
             )
         )
@@ -119,6 +120,7 @@ class TestSyntacticEMMOConventions(TestEMMOConventions):
                 "2-manifold",
                 "3-manifold",
                 "C++",
+                "3DPrinting",
             )
         )
         exceptions.update(self.get_config("test_class_label.exceptions", ()))
