cbfd12a58444dd9466607953283b6961e03c4794
Jesper Friis <jesper.friis@sintef.no>
2023-04-12 16:04:45 +0200

    Correct test to make it independent of being run interactively or not

--- tests/test_dir.py
+++ tests/test_dir.py
@@ -8,13 +8,16 @@ thisdir = Path(__file__).resolve().parent
 onto = get_ontology(
     thisdir / "test_excelparser/imported_onto/ontology.ttl"
 ).load()
-assert "TestClass2" in dir(onto)
-
 onto.dir_imported = False
+onto.dir_label = False
+onto.dir_name = False
 assert "TestClass2" not in dir(onto)
-assert "testclass" not in dir(onto)
 
 onto.dir_imported = True
+assert "TestClass2" in dir(onto)
+assert "testclass" not in dir(onto)
+assert "testclass2" not in dir(onto)
+
 onto.dir_name = True
 assert "TestClass2" in dir(onto)
 assert "testclass" in dir(onto)
