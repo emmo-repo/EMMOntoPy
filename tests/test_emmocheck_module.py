a60fc28d0b45ba49188ac4fe482b3d9ad4af2880
Jesper Friis <jesper.friis@sintef.no>
2023-10-09 11:19:09 +0200

    Test a specific commit of EMMO

--- tests/test_emmocheck_module.py
+++ tests/test_emmocheck_module.py
@@ -4,6 +4,7 @@ from emmopy.emmocheck import main
 main(
     argv=[
         "--url-from-catalog",
-        "https://raw.githubusercontent.com/emmo-repo/EMMO/1.0.0-beta5/emmo.ttl",
+        # Test against a specific commit of EMMO 1.0.0-beta5
+        "https://raw.githubusercontent.com/emmo-repo/EMMO/3b93e2c9c45ab8d9882d2d6385276ff905095798/emmo.ttl",
     ]
 )
