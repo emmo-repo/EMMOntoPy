1237e5cbad8b52168431e13b64859586090fcc7c
Jesper Friis <jesper.friis@sintef.no>
2023-11-23 12:57:44 +0100

    Updated test_emmocheck_module.py

--- tests/test_emmocheck_module.py
+++ tests/test_emmocheck_module.py
@@ -1,10 +1,13 @@
 from emmopy.emmocheck import main
 
 
-main(
+status = main(
     argv=[
         "--url-from-catalog",
+        "--check-imported",
         # Test against a specific commit of EMMO 1.0.0-beta5
         "https://raw.githubusercontent.com/emmo-repo/EMMO/3b93e2c9c45ab8d9882d2d6385276ff905095798/emmo.ttl",
     ]
 )
+
+assert status == 0
