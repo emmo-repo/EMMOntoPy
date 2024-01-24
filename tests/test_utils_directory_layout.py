c821d82aee343bc653d24aeca90477623b53d5ec
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2024-01-24 16:28:59 +0100

    Turned on directory layout test for emmo and made them pytet

--- tests/test_utils_directory_layout.py
+++ tests/test_utils_directory_layout.py
@@ -5,14 +5,15 @@ from ontopy import get_ontology
 from ontopy.utils import directory_layout
 
 
-# Requires that we have EMMO checked out locally
-if False:
-    emmo = get_ontology("../EMMO/emmo.ttl").load()
+def test_emmo_directory_layout():
+    emmo = get_ontology(
+        "https://raw.githubusercontent.com/emmo-repo/EMMO/1.0.0-beta4/emmo.ttl"
+    ).load()
     layout = directory_layout(emmo)
 
     # Map base IRIs to ontologies for easy access to all sub-ontologies
     omap = {o.base_iri: o for o in layout.keys()}
-
+    print(omap)
     # Base IRI of EMMO should not end with slash (/) !!!
     assert layout[omap["http://emmo.info/emmo/"]] == "emmo"
 
@@ -31,10 +32,6 @@ if False:
     assert (
         layout[omap["http://emmo.info/emmo/disciplines/math#"]]
         == "disciplines/math"
-    )
-    assert (
-        layout[omap["http://emmo.info/emmo/disciplines/units/siunits#"]]
-        == "disciplines/units/siunits"
     )
     assert (
         layout[omap["http://emmo.info/emmo/mereocausality#"]]
@@ -61,16 +58,12 @@ if False:
         == "disciplines/math"
     )
     assert (
-        layout[omap["http://emmo.info/emmo/disciplines/units/siunits#"]]
-        == "disciplines/units/siunits"
-    )
-    assert (
         layout[omap["http://emmo.info/emmo/mereocausality#"]]
         == "mereocausality/mereocausality"
     )
 
 
-if True:
+def test_local_directory_layout():
     thisdir = Path(__file__).resolve().parent
     ontopath = thisdir / "testonto" / "testonto.ttl"
     onto = get_ontology(ontopath).load()
