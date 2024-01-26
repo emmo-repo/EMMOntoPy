a332cb22520acc4d3d4568ead08c3acc86779921
francescalb <francesca.l.bleken@sintef.no>
2024-01-26 13:24:49 +0100

    Moved the squash test

--- tests/test_save.py
+++ tests/test_save.py
@@ -110,7 +110,27 @@ def test_save(
     )
     assert (tmpdir / "recursively" / "models.ttl").exists()
 
-    # squash merge during save is tested in test_ontology_squash.py
+
+def test_ontology_squash():
+    import re
+    from pathlib import Path
+    from ontopy import get_ontology
+
+    repo_dir = Path(__file__).resolve().parent.parent
+    onto_dir = repo_dir / "tests" / "testonto"
+    out_dir = repo_dir / "tests" / "output"
+
+    testonto = get_ontology(onto_dir / "testonto.ttl").load()
+
+    testonto.save(out_dir / "testonto_squash.ttl", squash=True)
+
+    with open(out_dir / "testonto_squash.ttl", "r") as f:
+        txt = f.read()
+
+    s = re.findall(r".* a owl:Ontology", txt)
+    assert len(s) == 1
+    assert s[0].startswith("<http://emmo.info/testonto>")
+    assert len(re.findall(r"owl:imports", txt)) == 0
 
 
 # Simple working tests without pytest getting in the way - feel free to change to pytest
