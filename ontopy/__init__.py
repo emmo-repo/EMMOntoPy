c8a9a6845db27971be6acce94a4a1a324fb49ac3
EMMOntoPy Developers <Team4.0@SINTEF.no>
2024-02-29 08:36:14 +0000

    Release v0.7.1 - Changelog

--- ontopy/__init__.py
+++ ontopy/__init__.py
@@ -2,7 +2,7 @@
 # pylint: disable=wrong-import-position,wrong-import-order
 import sys
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 # Ensure correct Python version
 if sys.version_info < (3, 7):
