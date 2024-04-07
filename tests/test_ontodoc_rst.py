7066c2d88c02f22248b978eaef7f89fd1a225dea
Jesper Friis <jesper.friis@sintef.no>
2024-04-07 16:14:38 +0200

    Cleaned up test_ontodoc_rst.py

--- tests/test_ontodoc_rst.py
+++ tests/test_ontodoc_rst.py
@@ -1,7 +1,8 @@
 """Test ontodoc"""
 
-if True:
-    # def test_ontodoc():
+
+# if True:
+def test_ontodoc():
     """Test ontodoc."""
     from pathlib import Path
 
@@ -9,8 +10,8 @@ if True:
     from ontopy.ontodoc_rst import OntologyDocumentation
     import owlready2
 
-    # emmo = get_ontology("https://w3id.org/emmo/1.0.0-rc1").load()
-    emmo = get_ontology("../EMMO/emmo.ttl").load()
+    emmo = get_ontology("https://w3id.org/emmo/1.0.0-rc1").load()
+    # emmo = get_ontology("../EMMO/emmo.ttl").load()
     # emmo.sync_reasoner(include_imported=True)
 
     od = OntologyDocumentation(
