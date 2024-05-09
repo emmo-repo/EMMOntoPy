587560662557b8240c8240888837d6ab7c77bcf0
Jesper Friis <jesper.friis@sintef.no>
2024-05-09 10:36:59 +0200

    Replaced EMMO with local animal ontology

--- tests/test_ontodoc_rst.py
+++ tests/test_ontodoc_rst.py
@@ -8,13 +8,14 @@ def test_ontodoc():
 
     from ontopy import get_ontology
     from ontopy.ontodoc_rst import OntologyDocumentation
+    from ontopy.testutils import ontodir
     import owlready2
 
-    emmo = get_ontology("https://w3id.org/emmo/1.0.0-rc1").load()
-    # emmo = get_ontology("../EMMO/emmo.ttl").load()
-    # emmo.sync_reasoner(include_imported=True)
+    # onto = get_ontology("https://w3id.org/emmo/1.0.0-rc1").load()
+    onto = get_ontology(ontodir / "animal.ttl").load()
+    # onto.sync_reasoner(include_imported=True)
 
     od = OntologyDocumentation(
-        emmo, recursive=True, iri_regex="https://w3id.org/emmo"
+        onto, recursive=True, iri_regex="https://w3id.org/emmo"
     )
     print(od.get_refdoc())
