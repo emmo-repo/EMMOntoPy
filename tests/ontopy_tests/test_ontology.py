6d83771168cdfd0833ddc0dfa07937dec7b0d8c5
Jesper Friis <jesper.friis@sintef.no>
2024-11-18 23:18:13 +0100

    Added find() method

---
+++ tests/ontopy_tests/test_ontology.py
@@ -0,0 +1,26 @@
+"""Test Ontology class methods."""
+
+from ontopy import get_ontology
+from ontopy.testutils import ontodir
+
+animal = get_ontology(ontodir / "animal.ttl").load()
+
+
+# if True:
+def test_find():
+    """Test find() method."""
+    m1 = (animal.chasing, animal.prefLabel, "chasing")
+    for domain in "ontology", "imported", "world":
+        assert m1 in animal.find("chas", domain=domain)
+        assert m1 in animal.find("chas", domain=domain, regex=True)
+        assert m1 in animal.find("chas", domain=domain, case_sensitive=True)
+        assert m1 in animal.find(
+            "chas", domain=domain, regex=True, case_sensitive=True
+        )
+
+        assert m1 in animal.find("Chas", domain=domain)
+        assert m1 in animal.find("Chas", domain=domain, regex=True)
+        assert m1 not in animal.find("Chas", domain=domain, case_sensitive=True)
+        assert m1 not in animal.find(
+            "Chas", domain=domain, regex=True, case_sensitive=True
+        )
