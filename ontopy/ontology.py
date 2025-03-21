efd1c02ab38552108b980bce694e1cf4ec1e20a0
Francesca L. Bleken <48128015+francescalb@users.noreply.github.com>
2025-03-21 18:45:09 +0100

    Update ontopy/ontology.py
    Co-authored-by: Jesper Friis <jesper-friis@users.noreply.github.com>

--- ontopy/ontology.py
+++ ontopy/ontology.py
@@ -2189,7 +2189,7 @@ class Ontology(owlready2.Ontology):  # pylint: disable=too-many-public-methods
             name: name of the data property
             parent: parent(s) of the data property
             iri: IRI of the new data property.  If None, a new IRI will be
-                based on the ontology base IRI and the entity name
+                based on the ontology base IRI and the entity name.
         Returns:
             the new data property.
         """
