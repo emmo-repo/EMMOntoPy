1bea0992375a4a8455068fefd34d0a12a7343ee0
Jesper Friis <jesper.friis@sintef.no>
2025-12-31 18:34:39 +0100

    Added test

--- tests/ontopy_tests/test_utils.py
+++ tests/ontopy_tests/test_utils.py
@@ -133,16 +133,26 @@ def test_rename_ontologies():
 
 
 def test_remove_owlready2_properties():
-    from ontopy.utils import remove_owlready2_properties
+    from ontopy.utils import rename_iris, remove_owlready2_properties
     from ontopy.testutils import get_testonto
-    
+
     onto = get_testonto()
-    onto.hasAnnotationProperty.python_name = "ap"
-    assert onto.hasAnnotationProperty.python_name == "ap"
-    
+    storid = onto._abbreviate(
+        "http://www.lesfleursdunormal.fr/static/_downloads/"
+        "owlready_ontology.owl#python_name"
+    )
+
+    # No initial owlready2 properties
+    assert not any(onto._get_triples_spod_spod(None, storid, None))
+
+    # Add owlready2 properties
+    rename_iris(onto)
+    assert any(onto._get_triples_spod_spod(None, storid, None))
+
+    # Remove owlready2 properties
     remove_owlready2_properties(onto)
-    assert onto.hasAnnotationProperty.python_name == "ap"
-    
+    assert not any(onto._get_triples_spod_spod(None, storid, None))
+
 
 def test_preferred_language():
     from ontopy import get_ontology
