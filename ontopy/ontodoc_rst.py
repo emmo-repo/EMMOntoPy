d69e820ba54f40a874929ebd4e42a2eff24dcabc
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2026-02-05 11:30:07 +0100

    Remove pyst exetnsion sources which ruined reference index

--- ontopy/ontodoc_rst.py
+++ ontopy/ontodoc_rst.py
@@ -757,11 +757,6 @@ extensions = [
     "myst_parser",
 ]
 
-source_suffix = {{
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}}
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
