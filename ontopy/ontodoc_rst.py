8ca39a0cf658a19d8e56f3ca91842a09c16be6b7
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2026-02-05 13:24:31 +0100

    Retry with correct source for sphinx

--- ontopy/ontodoc_rst.py
+++ ontopy/ontodoc_rst.py
@@ -757,6 +757,11 @@ extensions = [
     "myst_parser",
 ]
 
+source_suffix = {{
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}}
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
