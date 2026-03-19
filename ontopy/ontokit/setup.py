94a8c7734519cc317b8ca1b3ad5956aa6555f29f
Jesper Friis <jesper-friis@users.noreply.github.com>
2026-03-19 13:10:26 +0100

    New ontokit tool (#906)
    # 
    
    The new ontokit tool backbone setup. 
    
    This is included in master even though it is expected to change substantially in the near future because
    the current version is already in use by a few repos (domain-atomistic and domain-magnetic-materials).
    
    The two sub-commands that have been added are:
    ontokit setup : sets up the ontology repo with github workflows (which use ontokit docs)
    ontokit docs : runs the creation of reference documentation.
    
    ## Type of change
    <!-- Put an `x` in the box that applies. -->
    - [ ] Bug fix.
    - [ ] New feature.
    - [ ] Documentation update.
    - [ ] Test update.
    
    ## Checklist
    <!-- Put an `x` in the boxes that apply. You can also fill these out
    after creating the PR. -->
    
    This checklist can be used as a help for the reviewer.
    
    - [ ] Is the code easy to read and understand?
    - [ ] Are comments for humans to read, not computers to disregard?
    - [ ] Does a new feature has an accompanying new test (in the CI or unit
    testing schemes)?
    - [ ] Has the documentation been updated as necessary?
    - [ ] Does this close the issue?
    - [ ] Is the change limited to the issue?
    - [ ] Are errors handled for all outcomes?
    - [ ] Does the new feature provide new restrictions on dependencies, and
    if so is this documented?
    
    ## Comments
    <!-- Additional comments here, including clarifications on checklist if
    applicable. -->
    
    ---------
    
    Co-authored-by: Francesca L. Bleken <48128015+francescalb@users.noreply.github.com>
    Co-authored-by: Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>

---
+++ ontopy/ontokit/setup.py
@@ -0,0 +1,175 @@
+"""Module for the ontokit setup sub-command."""
+
+# pylint: disable=fixme
+
+import re
+import shutil
+import subprocess  # nosec
+from glob import glob
+from pathlib import Path
+from string import Template
+
+
+def _infer_github_repository(root, remote):
+    """Infer GitHub repository as 'owner/repo' from git remote URL."""
+    cmd = ["git", "-C", str(root), "remote", "get-url", remote]
+    proc = subprocess.run(  # nosec
+        cmd,
+        check=False,
+        capture_output=True,
+        text=True,
+    )
+    if proc.returncode != 0:
+        return None
+
+    remote_url = proc.stdout.strip()
+    patterns = (
+        r"^git@github\.com:(?P<owner>[^/]+)/(?P<repo>[^/]+?)(?:\.git)?$",
+        r"^https://github\.com/(?P<owner>[^/]+)/(?P<repo>[^/]+?)(?:\.git)?/?$",
+        r"^ssh://git@github\.com/(?P<owner>[^/]+)/(?P<repo>[^/]+?)(?:\.git)?/?$",  # pylint: disable=line-too-long
+    )
+    for pattern in patterns:
+        match = re.match(pattern, remote_url)
+        if match:
+            owner = match.group("owner")
+            repo = match.group("repo")
+            return f"{owner}/{repo}"
+
+    return None
+
+
+def setup_arguments(subparsers):
+    """Define arguments for the setup sub-command."""
+    parser = subparsers.add_parser(
+        "setup",
+        help=(
+            "Set up a repository with various github workflows, including "
+            "publishing of squashed and inferred ontologies, generation of "
+            "documentation, releases, etc..."
+        ),
+    )
+    parser.set_defaults(subcommand=setup_subcommand)
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Show Python traceback on error.",
+    )
+    parser.add_argument(
+        "root",
+        metavar="PATH",
+        help="Root folder of repository to setup.",
+    )
+    parser.add_argument(
+        "--ontology-name",
+        "-n",
+        metavar="NAME",
+        help=(
+            "Name of the ontology.  By default it is inferred from the base "
+            "name of the `root` folder."
+        ),
+    )
+    parser.add_argument(
+        "--ontology-prefix",
+        "-p",
+        metavar="PREFIX",
+        help=(
+            "Prefix for the ontology.  By default it is inferred from the "
+            "turtle file `{ONTOLOGY_NAME}.ttl`."
+        ),
+    )
+    parser.add_argument(
+        "--ontology-iri",
+        "-i",
+        metavar="IRI",
+        help=(
+            "IRI for the ontology.  By default it is inferred from the "
+            "turtle file `{ONTOLOGY_NAME}.ttl`."
+        ),
+    )
+    parser.add_argument(
+        "--github-pages-branch",
+        default="gh-pages",
+        metavar="NAME",
+        help="Name of the GitHub Pages branch [gh-pages]",
+    )
+    parser.add_argument(
+        "--remote",
+        default="origin",
+        metavar="NAME",
+        help="Remote git repository [origin]",
+    )
+    parser.add_argument(
+        "--github-repository",
+        "-g",
+        metavar="OWNER/REPO",
+        help=(
+            "GitHub repository in the form OWNER/REPO. "
+            "If omitted, inferred from --remote."
+        ),
+    )
+    parser.add_argument(
+        "--no-init",
+        action="store_true",
+        help="Do not try to initialise GitHub Pages branch.",
+    )
+
+
+def setup_subcommand(
+    args,
+):  # pylint: disable=too-many-locals,too-many-statements
+    """Implements the setup sub-command."""
+
+    thisdir = Path(__file__).resolve().parent
+    srcdir = thisdir / "setuptemplates"
+
+    root = Path(args.root).resolve()
+    github_dir = root / ".github"
+    workflows_dir = github_dir / "workflows"
+    scripts_dir = github_dir / "scripts"
+    workflows_dir.mkdir(mode=0o755, parents=True, exist_ok=True)
+
+    # TODO: infer ONTOLOGY_PREFIX and ONTOLOGY_IRI
+    ontology_name = args.ontology_name if args.ontology_name else root.name
+    ontology_prefix = args.ontology_prefix
+    ontology_iri = args.ontology_iri
+    github_repository = args.github_repository or _infer_github_repository(
+        root, args.remote
+    )
+
+    def ignore(src, names):
+        """Return file names to ignore when copying."""
+        # pylint: disable=unused-argument
+        return [name for name in names if name.endswith("~")]
+
+    shutil.copy(srcdir / "emmocheck_conf.yml", root / ".github")
+    shutil.copytree(
+        srcdir / "scripts",
+        root / ".github" / "scripts",
+        ignore=ignore,
+        dirs_exist_ok=True,
+    )
+
+    for fname in glob(str(srcdir / "workflows" / "*.yml")):
+        template = Template(Path(fname).read_text())
+        substituted = template.safe_substitute(
+            {
+                "ONTOLOGY_NAME": ontology_name,
+                "ONTOLOGY_PREFIX": ontology_prefix,
+                "ONTOLOGY_IRI": ontology_iri,
+                "GITHUB_REPOSITORY": github_repository or "",
+            }
+        )
+        outfile = workflows_dir / Path(fname).name
+        outfile.write_text(substituted)
+
+    # Initialise github pages branch
+    if not args.no_init:
+        args = [
+            scripts_dir / "init_ghpages.sh",
+            f"--ghpages={args.github_pages_branch}",
+            f"--ontology_name={ontology_name}",
+            f"--ontology_prefix={ontology_prefix}",
+            f"--ontology_iri={ontology_iri}",
+            f"--remote={args.remote}",
+        ]
+        subprocess.call(args, shell=True)  # nosec
