094d259c34b9e09a268a88967e07a91cc74464a0
Francesca L. Bleken <48128015+francescalb@users.noreply.github.com>
2026-03-19 15:34:26 +0100

    Update ontopy/ontokit/__init__.py

--- ontopy/ontokit/__init__.py
+++ ontopy/ontokit/__init__.py
@@ -8,7 +8,8 @@ __all__ = ["docs", "setup"]
 def __getattr__(name):
     """Lazily expose submodules for tooling such as mkdocstrings.
     
-    It will import the `docs` module when a user does `import ontokit.docs`. Similar for the other modules.
+    It will import the `docs` module when a user does `import ontokit.docs`. 
+    Similar for the other modules.
     """
     if name in __all__:
         module = import_module(f"{__name__}.{name}")
